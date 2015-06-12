ebe17ba41f1bbb6de395e37935eb9058d60a0edd
MacMladen iMac <mladen@bluefish.rs>
2015-06-12 12:29:36 +0200

    Created subthemes and added H5BP and Toolkit

---
+++ drupal/sites/all/themes/_custom/sassoncat/scripts/sassoncat.js
@@ -0,0 +1,18 @@
+/*
+ * #############################################
+ * ##### Sasson - Advanced Drupal Theming. #####
+ * #############################################
+ *
+ *           Sasson Cat main script.
+ */
+
+(function($) {
+
+  // DUPLICATE AND UNCOMMENT
+  // Drupal.behaviors.behaviorName = {
+  //   attach: function (context, settings) {
+  //     // Do some magic...
+  //   }
+  // };
+
+})(jQuery);
---
+++ drupal/sites/all/themes/_custom/sassoncat/template.php
@@ -0,0 +1,8 @@
+<?php
+
+// /**
+//  * Implements hook_preprocess_HOOK().
+//  */
+// function sassoncat_preprocess_HOOK(&$variables) {
+
+// }
