dd34ff96f6caceb6a3fc9e2b9c1c16eac0f4d8ae
MacMladen Buk3 <mladen@bluefish.rs>
2015-06-15 07:32:02 +0200

    Theming script improved, added AdvAgg module to help normalize all themes output to compressed form for fair comparison

--- theming-si.sh
+++ theming-si.sh
@@ -1,5 +1,5 @@
 # Install command - fails? run again
-drush -r drupal_root si standard \
+drush -r drupal_root/ si standard \
   --db-su=lokalni \
   --db-su-pw=lokalni \
   --db-url=mysql://lokalni:lokalni@localhost/theming \
@@ -17,12 +17,74 @@ chmod -R 777 drupal_root/sites/default/files
 drush -r drupal_root/ en -y adminimal_admin_menu
 
 # Activate all needed modules for themes
-drush en -y libraries html5_tools elements jquery_update magic
+drush -r drupal_root/ en -y libraries html5_tools elements jquery_update magic
 
 # Useful for some further style testing
-#drush en -y styleguide styleguide_palette
+#drush -r drupal_root/ en -y styleguide styleguide_palette
 
 # Generate test content, 20 pages, 20 articles
-drush en -y devel_generate
-drush generate-content 20 --types=page
-drush generate-content 20 --types=article
+drush -r drupal_root/ en -y devel_generate
+drush -r drupal_root/ generate-content 20 --types=page
+drush -r drupal_root/ generate-content 20 --types=article
+
+
+# Activate all needed modules for themes
+drush -r drupal_root/ en -y \
+  adaptivetheme \
+  arctica \
+  aurora \
+  basic \
+  bassline \
+  boilerplate \
+  bootstrap \
+  boron \
+  framework \
+  gesso \
+  groundwork \
+  kalatheme \
+  mothership \
+  om \
+  omega \
+  radix \
+  sasson \
+  titon \
+  tundra \
+  zen
+
+# Build drush subthemes 
+
+# Activate all starter themes
+# Activate all contrib themes
+drush -r drupal_root/ en -y \
+  bamboo \
+  bootstrap_clean_blog \
+  business \
+  business_responsive_theme \
+  corporateclean \
+  da_vinci \
+  gratis \
+  html5_simplified \
+  parrot \
+  responsive_bartik \
+  simpleclean \
+  touchpro \
+  zeropoint
+
+# Activate all needed modules for themes
+drush -r drupal_root/ en -y \
+  adaptive_cat \
+  aurora_cat \
+  bassline_cat \
+  boiler-cat \
+  boot_cat \
+  gesso_cat \
+  ground-cat \
+  kala_cat \
+  om_cat \
+  omega_cat \
+  omega_ohm \
+  radix_default \
+  radix_simplex \
+  radix_spacelab \
+  sassoncat \
+  zen_cat
