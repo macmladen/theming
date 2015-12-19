832343acce3a73d73b01b5c61a18ba0c1c0a02d4
MacMladen Buk3 <mladen@bluefish.rs>
2015-12-19 20:15:51 +0100

    Added theme developed for NBCOlympics (not fully developed but set some basics combined from Zen and Omega).

---
+++ drupal_root/sites/all/themes/_custom/nbcolympics/templates/block.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a block.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728246
+ */
+?>
+<section class="<?php print $classes; ?>"<?php print $attributes; ?> id="<?php print $block_html_id; ?>">
+  <?php print render($title_prefix); ?>
+  <?php if ($block->subject): ?>
+    <h2<?php print $title_attributes; ?>><?php print $block->subject; ?></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+  <?php print $content; ?>
+</section>
